1dcf3400ff888c6f4be7c372c55f2f7166a68402
Stefano Maiocchi <stefanomaiocchi@yahoo.com>
2019-04-06 21:45:39 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# reactsafari7
---
+++ precache-manifest.31245ea3de0dd9caa03a038dacb5d4e7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e150ce2e67c357d678c4fa8b6268fe2",
+    "url": "/reactsafari7/static/media/ANTILOPE0.8e150ce2.jpg"
+  },
+  {
+    "revision": "f0ed2d9b6a3b1fa9d790",
+    "url": "/reactsafari7/static/css/main.81366073.chunk.css"
+  },
+  {
+    "revision": "361af5a8c6232e2b7487",
+    "url": "/reactsafari7/static/js/runtime~main.f4b77e32.js"
+  },
+  {
+    "revision": "c48ad23ee9b645b559dd",
+    "url": "/reactsafari7/static/js/2.b73106d5.chunk.js"
+  },
+  {
+    "revision": "b0deba22c96bbb6c773197b44fbca818",
+    "url": "/reactsafari7/static/media/GIRAFFA0.b0deba22.jpg"
+  },
+  {
+    "revision": "ea517e8febee68ae63769a326b96a980",
+    "url": "/reactsafari7/static/media/ROOM2.ea517e8f.png"
+  },
+  {
+    "revision": "550d33924bd1339a0ef1b87ca051150e",
+    "url": "/reactsafari7/static/media/GHEPARDO0.550d3392.jpg"
+  },
+  {
+    "revision": "f0ed2d9b6a3b1fa9d790",
+    "url": "/reactsafari7/static/js/main.cf5fda8c.chunk.js"
+  },
+  {
+    "revision": "cf145cef2cb18566d719c11b9456f544",
+    "url": "/reactsafari7/static/media/LION0.cf145cef.jpeg"
+  },
+  {
+    "revision": "00954702521da06b8502544f8912aa47",
+    "url": "/reactsafari7/static/media/WILDBEAST0.00954702.jpg"
+  },
+  {
+    "revision": "57ce618a0bd530d9ca7b64c9cc1f341b",
+    "url": "/reactsafari7/static/media/TENT1.57ce618a.png"
+  },
+  {
+    "revision": "7d3952cbdc9b52edd1ac1509967ddd12",
+    "url": "/reactsafari7/static/media/TENT2.7d3952cb.png"
+  },
+  {
+    "revision": "6dbb459f49a6a9fc7b79c3d8fdde3604",
+    "url": "/reactsafari7/static/media/TENT3.6dbb459f.png"
+  },
+  {
+    "revision": "da748761bcaff3e3cf49803729ade3d9",
+    "url": "/reactsafari7/static/media/HomeBG.da748761.png"
+  },
+  {
+    "revision": "571ad27cbd2cc2ad7b5d128061f09713",
+    "url": "/reactsafari7/index.html"
+  }
+];
